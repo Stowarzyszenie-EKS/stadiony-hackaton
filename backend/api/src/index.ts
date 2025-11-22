a372c8ca42c6cb89cb8bcdcd763afe13a26329b7
fpysz@volvocars.com <fpysz@volvocars.com>
2025-11-22 01:25:27 +0100

    huj

--- backend/api/src/index.ts
+++ backend/api/src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { Client } from "pg";
+import {Client} from "pg";
 
 const app = express();
 const PORT = 8080;
@@ -12,8 +12,8 @@ const stadiumData = {
     clubName: "FCSuperClub"
 };
 
-app.get("/seats/:clubName/:eventId", async (req, res) => {
-    const { clubName, eventId } = req.params;
+app.get("/seats/:clubName/", async (req, res) => {
+    const {clubName} = req.params;
     const client = new Client({
         host: "app-db",
         user: "app",
@@ -22,21 +22,35 @@ app.get("/seats/:clubName/:eventId", async (req, res) => {
     });
     try {
         await client.connect();
+        const latestEventResult = await client.query(
+            `SELECT event_id
+             FROM events
+             WHERE club_name = $1
+             ORDER BY timestamp DESC LIMIT 1`,
+            [clubName]
+        );
+        if (latestEventResult.rows.length === 0) {
+            res.status(404).json({error: "No events found for this club"});
+            return;
+        }
+        const eventId = latestEventResult.rows[0].event_id;
+
         const result = await client.query(
             `SELECT timestamp, capacity, available_places
              FROM events
-             WHERE club_name = $1 AND event_id = $2
+             WHERE club_name = $1
+               AND event_id = $2
              ORDER BY timestamp DESC`,
             [clubName, eventId]
         );
         if (result.rows.length === 0) {
-            res.status(404).json({ error: "Event not found" });
+            res.status(404).json({error: "Event not found"});
         } else {
             res.status(200).json(result.rows);
         }
     } catch (err) {
         const errorMessage = err instanceof Error ? err.message : String(err);
-        res.status(500).json({ error: "Database error", details: errorMessage });
+        res.status(500).json({error: "Database error", details: errorMessage});
     } finally {
         await client.end();
     }
